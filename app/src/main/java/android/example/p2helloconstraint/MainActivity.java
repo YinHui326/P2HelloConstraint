29df75c0899b4571d8738c2fca7388df141b897e
Yin <tanyh-wm19@student.tarc.edu.my>
2020-07-01 17:33:00 +0800

    commit by YH

--- app/src/main/java/android/example/p2helloconstraint/MainActivity.java
+++ app/src/main/java/android/example/p2helloconstraint/MainActivity.java
@@ -1,32 +1,72 @@
-
 package android.example.p2helloconstraint;
 
-        import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
-        import android.example.p2helloconstraint.R;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.TextView;
-        import android.widget.Toast;
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private int mcount = 0;
+    private int mCount = 0;
     private TextView mShowCount;
+    private Button mZero;
+    private Button mEvenOdd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        mZero = (Button) findViewById(R.id.button_zero);
+        mEvenOdd = (Button) findViewById(R.id.button_count);
     }
 
-    public void showToast(View view){
-        Toast toast= Toast.makeText(this, R.string.toast_message,Toast.LENGTH_SHORT);
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,Toast.LENGTH_SHORT);
+
         toast.show();
     }
 
-    public void countUp(View view){
-        ++mcount;
-        if (mShowCount !=null)
-            mShowCount.setText(Integer.toString(mcount));
+    @SuppressLint("SetTextI18n")
+    public void countUp(View view) {
+        if(mCount == 0) {
+            mZero.setBackgroundColor(Color.MAGENTA);
+        }
+        mCount++;
+        if(mCount%2 == 0) {
+            mEvenOdd.setBackgroundColor(Color.BLUE);
+        }
+        if(mCount%2 == 1) {
+            mEvenOdd.setBackgroundColor(Color.GREEN);
+        }
+        if(mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+    public void reset(View view) {
+        mCount = 0;
+        mZero.setBackgroundColor(Color.LTGRAY);
+        mEvenOdd.setBackgroundColor(Color.BLUE);
+        if(mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
